3ea852c16e59b7b11cf6c322bb6edf7efa35567c
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-15 19:19:59 +0100

    Delete button. Adding live search.

--- src/components/App.js
+++ src/components/App.js
@@ -26,7 +26,7 @@ class App extends Component {
     })
   }
 
-  handleCitySubmit = (e) => {
+  /*handleCitySubmit = (e) => {
     e.preventDefault()
     console.log('potwierdzony formularz')
     const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`;
@@ -60,6 +60,43 @@ class App extends Component {
         city: this.state.value,
       })
     })
+  }*/
+
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.value.length === 0) return
+    if (prevState.value !== this.state.value){
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`;
+
+      fetch(API)
+      .then(response => {
+        if(response.ok){
+          return response
+        }
+        throw Error("Nie udało się")
+      })
+      .then(response => response.json())
+      .then(data => {
+        const day = new Date().toLocaleDateString()
+        const time = new Date().toLocaleTimeString()
+        this.setState({
+          day,
+          time,
+          city: this.state.value,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: (data.main.temp - 273,15),
+          pressure: data.main.pressure,
+          wind: data.wind.speed,
+          error: false,
+        })
+      })
+      .catch(error => {
+        this.setState({
+          error: true,
+          city: this.state.value,
+        })
+      })
+    }
   }
 
   render() {
@@ -67,10 +104,9 @@ class App extends Component {
       <div className="App">
         <Form 
           value={this.state.value} 
-          change={this.handleInputChange}
-          submit={this.handleCitySubmit}  
+          change={this.handleInputChange} 
         />
-        <Result weather={this.state}/>
+        <Result weather={this.state} />
       </div>
     );
   }

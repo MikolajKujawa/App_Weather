0b1577cab606b1930f8686c20600339b95f03f23
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-15 22:45:48 +0100

    update app

--- src/components/App.js
+++ src/components/App.js
@@ -26,42 +26,6 @@ class App extends Component {
     })
   }
 
-  /*handleCitySubmit = (e) => {
-    e.preventDefault()
-    console.log('potwierdzony formularz')
-    const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`;
-
-    fetch(API)
-    .then(response => {
-      if(response.ok){
-        return response
-      }
-      throw Error("Nie udało się")
-    })
-    .then(response => response.json())
-    .then(data => {
-      const day = new Date().toLocaleDateString()
-      const time = new Date().toLocaleTimeString()
-      this.setState({
-        day,
-        time,
-        city: this.state.value,
-        sunrise: data.sys.sunrise,
-        sunset: data.sys.sunset,
-        temp: (data.main.temp - 273,15),
-        pressure: data.main.pressure,
-        wind: data.wind.speed,
-        error: false,
-      })
-    })
-    .catch(error => {
-      this.setState({
-        error: true,
-        city: this.state.value,
-      })
-    })
-  }*/
-
   componentDidUpdate(prevProps, prevState){
     if (this.state.value.length === 0) return
     if (prevState.value !== this.state.value){

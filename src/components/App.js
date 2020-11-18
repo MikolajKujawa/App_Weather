30b5df927a258b3258ae2a8597a5e567299eeec7
Mikołaj Kujawa <mkkujawix@gmail.com>
2020-11-18 19:35:15 +0100

    change API

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   componentDidUpdate(prevProps, prevState){
     if (this.state.value.length === 0) return
     if (prevState.value !== this.state.value){
-      const API = `https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`;
+      const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&appid=${APIKey}`;
 
       fetch(API)
       .then(response => {
